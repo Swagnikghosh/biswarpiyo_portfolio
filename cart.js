a9adf32d03dfa8ec31319fb811fd35f67c317c0e
Swagnik Ghosh <swagnikghosh705@gmail.com>
2024-06-07 02:02:17 +0530

    beta

--- cart.js
+++ cart.js
@@ -1,22 +1,22 @@
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
+let previewContainer = document.querySelector('.products-preview');
+let previewBox = previewContainer.querySelectorAll('.preview');
 
-document.querySelectorAll('.products-container .product').forEach(product =>{
-  product.onclick = () =>{
-    preveiwContainer.style.display = 'flex';
+document.querySelectorAll('.products-container .product').forEach(product => {
+  product.onclick = () => {
+    previewContainer.style.display = 'flex';
     let name = product.getAttribute('data-name');
-    previewBox.forEach(preview =>{
+    previewBox.forEach(preview => {
       let target = preview.getAttribute('data-target');
-      if(name == target){
+      if (name == target) {
         preview.classList.add('active');
       }
     });
   };
 });
 
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
+previewBox.forEach(close => {
+  close.querySelector('.fa-times').onclick = () => { // Corrected selector here
     close.classList.remove('active');
-    preveiwContainer.style.display = 'none';
+    previewContainer.style.display = 'none';
   };
 });
